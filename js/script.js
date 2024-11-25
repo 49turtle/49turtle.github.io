dc088d81671f30bf2ed323e76f8ec9fd8b80af35
배성진 <baesung49@naver.com>
2024-11-25 13:12:49 +0900

    Update githubio

--- js/script.js
+++ js/script.js
@@ -15,7 +15,7 @@ function openModal(projectId) {
             <h3>프로젝트 개요</h3>
             <ul>
               <li><strong>목표:</strong> 개인 맞춤형 건강보조식품 추천 플랫폼 개발</li>
-              <li><strong>주요 기능:</strong> 비타민, 무기질 영양성분 데이터 기반 영양제 추천</li>
+              <li><strong>주요 기능:</strong> 영양성분 데이터 기반 영양제 추천, 관리자 페이지 CRUD 기능</li>
             </ul>
           </div>
           <div class="modal-right">
@@ -32,20 +32,21 @@ function openModal(projectId) {
           </section>
           <section>
             <h3>담당 역할</h3>
-            <p>프론트엔드 주요 기능 개발: sessionStorage 활용한 유저 상태 유지, 동적 헤더 UI, 데이터 페이지네이션 구현</p>
-            <p>크롤링 데이터를 가공해 단위 변환 및 관리자 액션 연동</p>
+            <p><strong>DB 설계 및 생성, 프론트엔드 메인</strong></p>
+            <p><strong>데이터 크롤링</strong></p>
           </section>
           <section>
             <h3>구현 사항</h3>
             <p><strong>프론트-백엔드 연동:</strong> 데이터 기반 추천 시스템을 위한 API 개발 및 테스트</p>
-            <p><strong>데이터 처리:</strong> 대규모 영양제 데이터를 효율적으로 관리 및 시각화</p>
-            <img src="./assets/img/vitamine_comb.gif" alt="영양제 조합 분석" class="modal-image" width=70%/>
+            <p><strong>프론트엔드 주요 기능 개발:</strong> sessionStorage 활용한 유저 상태 유지, 동적 헤더 UI, 데이터 페이지네이션 구현</p>
+            <p><strong>데이터 수집 및 처리:</strong> 데이터를 크롤링 및 가공해 단위 변환 및 SQL문 생성 자동화 코드 구현</p>
+            <img src="./assets/img/vitamine_comb.gif" alt="영양제 조합 분석" class="modal-image"/>
             <p class="caption">VitaMine: 내 영양제 조합 분석</p>
           </section>
           <section>
             <h3>문제 해결 사례</h3>
-            <p><strong>문제:</strong> 대량 데이터 처리 속도 저하 및 UI 업데이트 지연</p>
-            <p><strong>해결:</strong> Pinia와 Vue.js의 상태 관리 라이브러리로 UI 상태 갱신 최적화</p>
+            <p><strong>문제: 운영체제 간 환경 차이 및 사이트 크롤링 차단</strong> <br/> Selenium을 사용하여 데이터 수집을 진행하는 과정에서, Windows와 macOS 간 파일 경로 문제 및 Chromedriver 버전 차이로 인해 크롤링 코드 실행에 오류가 발생했습니다. 또한, 크롤링 대상 사이트에서 자동화 탐지 및 차단(로봇 검사)으로 인해 일부 데이터를 수집할 수 없었습니다. </p>
+            <p><strong>해결: 운영체제 호환성 확보</strong> <br/> 크롬 드라이버 경로 및 권한 문제를 해결하기 위해 각 OS에 맞는 드라이버 설치 및 설정을 수행하였고, macOS에서의 보안 경고를 해제하기 위해 xattr -d com.apple.quarantine 명령을 사용했습니다. 이를 통해 동일한 코드를 두 환경에서 모두 실행 가능하도록 했습니다.</p>
           </section>
         </div>
       </div>
@@ -71,19 +72,21 @@ function openModal(projectId) {
         </div>
         <div class="modal-footer">
           <section>
-            <h3>담당 역할</h3>
-            <p>YOLO 모델을 활용해 관절 포인트를 추출하고, LSTM으로 자세를 분석 및 분류</p>
-            <p>GPU 환경(CUDA, CuDNN) 구축 및 데이터 학습 시간 단축</p>
+            <h3>사용 기술</h3>
+            <p><strong>Mobile:</strong> React-Native-cli: 2.0.1, React-Native 0.74.3, NodeJS v8.20.4 LTS, Android Studio Koala.</p>
+            <p><strong>Back-end:</strong> Oracle Java 17.0.12, Spring boot 3.3.1, SpringSecurity 6.3.1, JPA, Swagger 2.9.2, MariaDB 10.3.23</p>
+            <p><strong>AI:</strong> python 3.10.14, FAST-API 0.111.1, tensorflow 2.12.0</p>
+            <p><strong>AIoT:</strong> Jetson Orin Nano Dev-Kit, Python3.8.10, YOLOv8n-pose, YOLOv8s, OpenCV</p>
           </section>
           <section>
-            <h3>사용 기술</h3>
-            <p><strong>Back-end:</strong> Python, FastAPI</p>
-            <p><strong>AI:</strong> YOLO, LSTM, TensorFlow</p>
+            <h3>담당 역할</h3>
+            <p><strong>AI 모델 선정: </strong>달리기 자세를 평가하기 위한 AI 모델 선정</p>
+            <p><strong>AI 모델 전개: </strong>FastAPI를 통한 AI 모델 서빙 API 코드 작성</p>
           </section>
           <section>
             <h3>구현 사항</h3>
-            <p><strong>실시간 분석:</strong> 영상 데이터를 실시간으로 분석하여 사용자의 자세 피드백 제공</p>
-            <p><strong>시각화:</strong> 분석 데이터를 시각적으로 표현하여 협업 간 이해도 향상</p>
+            <p><strong>실시간 AI 분석:</strong> 영상 데이터를 실시간으로 분석하여 사용자의 자세 피드백 제공, YOLO 모델을 활용해 관절 포인트를 추출하고, LSTM으로 자세를 분석 및 분류, GPU 환경(CUDA, CuDNN) 구축 및 데이터 학습 시간 단축</p>
+            <p><strong>데이터 시각화:</strong> 분석 데이터를 시각적으로 표현하여 협업 간 이해도 향상</p>
             <img src="./assets/img/posemate.gif" alt="POSEMATE 데이터 시각화" class="modal-image" width=250px/>
           </section>
           <section>
@@ -115,27 +118,28 @@ function openModal(projectId) {
         </div>
         <div class="modal-footer">
           <section>
-            <h3>담당 역할</h3>
-            <p>React를 활용한 UI 설계 및 Wake Lock API로 모바일 UX 개선</p>
-            <p>AudioContext API를 활용한 음성 시각화 및 데이터 연동</p>
+            <h3>사용 기술</h3>
+            <p><strong>Front-end:</strong> React, typescript, styled-component, React Query, three.js, PWA</p>
+            <p><strong>Back-end:</strong> Java(17), JDK(17.0.10), Spring Boot(3.3.3), JPA, MySQL(8.038), Redis</p>
+            <p><strong>AI:</strong> FastAPI, Hugging Face, Jupyter Notebook</p>
+            <p><strong>Infra:</strong> AWS EC2, Ubuntu 22.04 (LTS), Docker, Nginx, Jenkins</p>
           </section>
           <section>
-            <h3>사용 기술</h3>
-            <p><strong>Front-end:</strong> React, TypeScript, Styled Components</p>
-            <p><strong>Back-end:</strong> FastAPI</p>
-            <p><strong>AI:</strong> Kobart, HuggingFace</p>
+            <h3>담당 역할</h3>
+            <p><strong>AI 개발: </strong>LDA를 통한 토픽 모델링, KoBart 모델을 활용한 AI 뉴스 요약</p>
+            <p><strong>프론트엔드 개발: </strong>모바일 기능 구현 위한 PWA 설정, 외부 라이브러리를 활용한 페이지 구현으로 UX 향상</p>
           </section>
           <section>
             <h3>구현 사항</h3>
-            <p>모바일 환경에서 알람 및 브리핑 서비스를 원활히 제공</p>
-            <p>AI 요약 데이터를 바탕으로 사용자의 관심사를 반영한 브리핑 생성</p>
-            <img src="./assets/img/honeymorning_lda.png" alt="Honey Morning LDA" class="modal-image" width=70%/>
+            <p>React를 활용한 UI 설계 및 PWA 설정, Wake Lock API로 모바일 UX 개선, 모바일 환경에서 알람 및 브리핑 서비스를 원활히 제공</p>
+            <p>네이버 뉴스 기사 페이지에서 정치, 경제, 사회, 생활/문화, IT/기술, 세계의 6개 카테고리에 대해 BS4를 이용한 정적 크롤링을 진행, Kobart 모델을 통한 AI 요약 데이터를 바탕으로 사용자의 관심사를 반영한 브리핑 생성</p>
+            <img src="./assets/img/honeymorning_lda.png" alt="Honey Morning LDA" class="modal-image"/>
             <p class="caption">pyLDAvis로 시각화한 LDA 토픽 모델링 HTML</p>
           </section>
           <section>
             <h3>문제 해결 사례</h3>
-            <p><strong>문제:</strong> 사용자 맞춤형 브리핑 생성 시 처리 속도 저하</p>
-            <p><strong>해결:</strong> Kmeans 알고리즘 적용으로 데이터 선택 및 처리 최적화</p>
+            <p><strong>문제:</strong> 수집된 뉴스 기사가 카테고리당 최대 160개 (알람 시간 이전 8시간, 1시간당 최대 20개의 뉴스 기사 크롤링)이기 때문에, 해당 뉴스를 모두 요약하는 것은 자원의 낭비가 심했습니다.</p>
+            <p><strong>해결:</strong> 한 카테고리 당 대표 기사 3개를 뽑아 요약 모델의 input으로 사용, 데이터 자체를 클러스터의 중심으로 출력해주는 KMedoids 알고리즘을 이용해 대표 기사를 선택</p>
           </section>
         </div>
       </div>
@@ -166,25 +170,53 @@ function openModal(projectId) {
       <!-- 추가 정보 -->
       <div class="modal-footer">
         <section>
+          <h3>사용 기술</h3>
+            <p><strong>Front-end:</strong></p>
+            <img src="https://img.shields.io/badge/Node.js-18.20.4-339933?logo=node.js&logoColor=white">
+            <img src="https://img.shields.io/badge/Axios-1.5.2-5A29E4?logo=axios&logoColor=white">
+            <img src="https://img.shields.io/badge/React-18.3.1-61DAFB?logo=react&logoColor=white">
+            <img src="https://img.shields.io/badge/Redux_Toolkit-2.3.0-764ABC?logo=redux&logoColor=white">
+            <img src="https://img.shields.io/badge/React_Query-4.29.5-FF4154?logo=react-query&logoColor=white">
+            <img src="https://img.shields.io/badge/React_Router-6.15.0-CA4245?logo=react-router&logoColor=white">
+            <img src="https://img.shields.io/badge/TailwindCSS-3.4.14-06B6D4?logo=tailwindcss&logoColor=white">
+            <img src="https://img.shields.io/badge/Styled_Components-6.0.8-DB7093?logo=styled-components&logoColor=white">
+            <img src="https://img.shields.io/badge/three.js-0.170.0-000000?logo=three.js&logoColor=white">
+            <img src="https://img.shields.io/badge/WebSocket-Standard-010101?logo=websocket&logoColor=white">
+
+            <p><strong>Back-end:</strong></p>
+            <img src="https://img.shields.io/badge/Java-17.0.2-007396?logo=java&logoColor=white">
+            <img src="https://img.shields.io/badge/Spring_Boot-3.3.4-6DB33F?logo=spring-boot&logoColor=white">
+            <img src="https://img.shields.io/badge/MySQL-8.2.0-4479A1?logo=mysql&logoColor=white">
+            <img src="https://img.shields.io/badge/JWT-4.4.0-000000?logo=json-web-tokens&logoColor=white">
+
+            <p><strong>AI:</strong></p>
+            <img src="https://img.shields.io/badge/LangChain-0.3.7-000000">
+            <img src="https://img.shields.io/badge/FastAPI-0.115.4-009688?logo=fastapi&logoColor=white">
+            <img src="https://img.shields.io/badge/Selenium-4.26.1-43B02A?logo=selenium&logoColor=white">
+
+            <p><strong>Infra:</strong></p>
+            <img src="https://img.shields.io/badge/AWS_EC2-t2.xlarge-FF9900?logo=amazon-aws&logoColor=white">
+            <img src="https://img.shields.io/badge/AWS_EC2-c6g.2xlarge-FF9900?logo=amazon-aws&logoColor=white">
+            <img src="https://img.shields.io/badge/Ubuntu-20.04.6_LTS-E95420?logo=ubuntu&logoColor=white">
+            <img src="https://img.shields.io/badge/Docker-27.3.1-2496ED?logo=docker&logoColor=white">
+            <img src="https://img.shields.io/badge/Jenkins-2.462.3-D24939?logo=jenkins&logoColor=white">
+            <img src="https://img.shields.io/badge/Nginx-1.18.0-009639?logo=nginx&logoColor=white">
+
+        </section>
+        <section>
           <h3>담당 역할</h3>
-          <p><strong>AI 개발</strong>: LangChain을 활용한 차트 패턴 분석, 뉴스 검색, 의사결정 AI 에이전트 개발</p>
+          <p><strong>AI 개발</strong>: LangChain을 활용한 차트 패턴 분석, 뉴스 검색, 의사결정 3개의 AI 에이전트 개발 담당</p>
           <p><strong>프론트엔드 AI 연동</strong>: 차트 패턴 분석 결과 연동 및 거래 및 상세 전략 페이지 모달 창 구현</p>
         </section>
         <section>
-          <h3>사용 기술</h3>
-            <p><strong>AI:</strong> LangChain, GPT-4o</p>
-            <p><strong>Back-end:</strong> Python, FastAPI </p>
-            <p><strong>Front-end:</strong> React, TypeScript, styled-components, Three.js</p>
-        </section>
-        <section>
           <h3>구현 사항</h3>
-          <p><strong>RAG 및 개인화 에이전트 구현</strong>: 벡터 저장소를 활용한 투자 성향 문서 임베딩, 사용자가 입력한 투자 성향 문장과 가장 유사한 투자 성향 문서를 찾아 해당 성향을 투자에 적용하는 개인화 에이전트 구현</p>
-          <p><strong>멀티모달 AI 활용</strong>: 차트 패턴 분석을 위해 GPT-4 Vision을 활용, ARM Linux 서버 환경에서의 차트 캡쳐 자동화를 위한 Selenium 실행 코드 구현</p>
-          <img src="./assets/img/trai_flow.png" alt="TRAI 플로우" class="modal-image" width=60%/>
+          <p><strong>RAG 및 개인화 에이전트 구현</strong>: FAISS 벡터 저장소를 활용한 투자 성향 문서 임베딩, 사용자가 입력한 투자 성향 문장과 가장 유사한 투자 성향 문서를 찾아 해당 성향을 투자에 적용하는 개인화 에이전트 구현</p>
+          <p><strong>멀티모달 AI 활용</strong>: 차트 패턴 분석을 위해 GPT-4 Vision을 활용, ARM Linux 서버 환경에서의 차트 캡쳐 자동화를 위한 Selenium 실행 코드 및 환경 구축</p>
+          <img src="./assets/img/trai_agent.png" alt="TRAI 에이전트" class="modal-image" />
         </section>
         <section>
           <h3>문제 해결 사례</h3>
-          <p><strong>문제:</strong> 오픈소스 LLM의 비용과 프롬프트 최적화 및 기술적 분석 AI 에이전트의 환각 현상</p>
+          <p><strong>문제:</strong> 오픈소스 LLM의 비용과 프롬프트 최적화 및 기술적 분석 AI 에이전트의 환각 현상 발생</p>
           <p><strong>해결:</strong> LangSmith를 활용한 지연 시간 및 사용 토큰량 추적, 기술적신호(SMA, MACD, RSI 등) 계산 함수로 전처리 후 프롬프트에 동적으로 삽입</p>
         </section>
         <section>
